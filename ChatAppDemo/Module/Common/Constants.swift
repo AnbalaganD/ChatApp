1ae5f030e5ae22203d456a44365d47b0dba4a7e5
Anbalagan D <anbu94p@gmail.com>
2018-06-16 10:04:49 +0530

    System Design

---
+++ ChatAppDemo/Module/Common/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ChatAppDemo
+//
+//  Created by Admin on 16/06/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    let appName = "ChatApp"
+}
